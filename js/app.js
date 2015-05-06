3adb8a28c4503c9416607404869710b9302eb3d6
Samuel Moore <semoore95@gmail.com>
2015-05-05 20:40:17 -0400

    Document/Comment app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 // app.js
 
-var configureGridItemHover = function( img ) {
+/* configureGridItemHover
+ *
+ * configures an `img` element in our Grid use a Color.png on hover.
+ */
+this.configureGridItemHover = function( img ) {
   hover_url = img.attr('src').split("BW.png").join("Color.png");
 
   img.hoverImg(hover_url);
 };
 
-var configureGridItemPlayback = function ( a ) {
+/* configureGridItemPlayback
+ *
+ * configures an `a` element to open magnific-popup with its video URL.
+ */
+this.configureGridItemPlayback = function ( a ) {
   a.magnificPopup({
     disableOn: 700,
     type: 'iframe',
@@ -26,89 +34,112 @@ var configureGridItemPlayback = function ( a ) {
   });
 };
 
-var configureGrid = function( selector ) {
+/* configureStretchToFitVideo
+ *
+ * configures a video tag that matches the selector
+ * to stretch to the size of its container, rather than
+ * maintaining aspect ratio as per W3 Specifications.
+ */
+this.configureStretchToFitVideo = function( selector ) {
+	var $video = $(selector),
+	    videoRatio = selector.videoWidth / selector.videoHeight,
+	    tagRatio = $video.width() / $video.height();
+	if (videoRatio < tagRatio) {
+	  $video.css('-webkit-transform','scaleX(' + tagRatio / videoRatio  + ')')
+	  $video.css('-moz-transform','scaleX(' + tagRatio / videoRatio  + ')')
+	  $video.css('-ms-transform','scaleX(' + tagRatio / videoRatio  + ')')
+	  $video.css('transform','scaleX(' + tagRatio / videoRatio  + ')')
+	} else if (tagRatio < videoRatio) {
+	  $video.css('-webkit-transform','scaleY(' + videoRatio / tagRatio  + ')')
+	  $video.css('-moz-transform','scaleY(' + videoRatio / tagRatio  + ')')
+	  $video.css('-ms-transform','scaleY(' + videoRatio / tagRatio  + ')')
+	  $video.css('transform','scaleY(' + videoRatio / tagRatio  + ')')
+	}
+};
+
+/* configureGrid
+ *
+ * Takes a selector which matches a DOM element which contains many:
+ *
+ * <div class="item">
+     <a href="LINK_TO_VIDEO_OR_OTHER">
+       <img src="path/to/anImage BW.png">
+     </a>
+   </div>
+ *
+ * and triggers configuration for the `a` and `img` element respectively.
+ */
+this.configureGrid = function( selector ) {
   items = $(selector).find('.item');
 
   $.each(items, function () {
-    item = $(this);
+    $item = $(this);
 
-    a = item.find('a').first();
+    a = $item.find('a').first();
     configureGridItemPlayback(a);
 
-    img = item.find('img').first();
+    img = $item.find('img').first();
     configureGridItemHover(img);
   });
 };
 
-var configureSocial = function( selector ) {
-  $(selector).each(function () {
-    var $img = $(this).find('img').first();
-    //hover_url = $img.attr('src').split('.png').join(' hover.png');
-    // $img.hoverImg(hover_url);
-  });
-};
-
+// ===================
+// ==   ON READY   ===
+// ===================
 $(document).ready(function () {
+  // configure mobile menu
   $("#menu").sidr();
   
+  // configure `a` and `img` tags in the grid.
   configureGrid('.work-section');
   configureGrid('.impact-section');
 
-  configureSocial('.contact-section ul li');
+  // ------------
+  // SUB-NAV CODE
+  // ------------
 
+  // selector of `a` tags that can trigger the subnav
   toggleNavSelector = 'a#toggleNav';
 
-  $(toggleNavSelector).click(function () {
+  $(toggleNavSelector).click(function () 
+  {
+    // 1. cache whether the corresponding container for clicked link is hidden
     selector = '.' + $(this).attr('href').split('#').join('');
     isHidden = $(selector).hasClass('hidden');
 
+    // 2. now hide all containers that can be toggled 
     togglers = $(toggleNavSelector);
     togglers.each(function () {
       eachSelector = '.' + $(this).attr('href').split('#').join('');
       $(eachSelector).toggleClass('hidden', true);
     });
 
+    // 3. if the clicked container was hidden, we then show it.
     if (isHidden == true) {
       $(selector).toggleClass('hidden');
     }
   });
 
-  /* RESIZE VIDEO CODE */
+  // ------------
+  // SPLASH VIDEO
+  // ------------
 
-  var ResizeVideo = function(videoTag) {
-    console.log(videoTag);
-    var $video = $(videoTag),
-        videoRatio = videoTag.videoWidth / videoTag.videoHeight,
-        tagRatio = $video.width() / $video.height();
-    if (videoRatio < tagRatio) {
-      $video.css('-webkit-transform','scaleX(' + tagRatio / videoRatio  + ')')
-      $video.css('-moz-transform','scaleX(' + tagRatio / videoRatio  + ')')
-      $video.css('-ms-transform','scaleX(' + tagRatio / videoRatio  + ')')
-      $video.css('transform','scaleX(' + tagRatio / videoRatio  + ')')
-    } else if (tagRatio < videoRatio) {
-      $video.css('-webkit-transform','scaleY(' + videoRatio / tagRatio  + ')')
-      $video.css('-moz-transform','scaleY(' + videoRatio / tagRatio  + ')')
-      $video.css('-ms-transform','scaleY(' + videoRatio / tagRatio  + ')')
-      $video.css('transform','scaleY(' + videoRatio / tagRatio  + ')')
-    }
-  };
+  $video = $('.stretchToFit');
 
   $(window).resize(function() {
-    $('.stretchToFit').each(function() {
+    $video.each(function() {
       var CurrentChild = this;
-      ResizeVideo(CurrentChild);
+      configureStretchToFitVideo(CurrentChild);
     });
   });
 
-  $('video').bind("loadeddata", function(anEvent) {
-    var loadedVideoElement = anEvent.target;
-    if ($(loadedVideoElement).hasClass('stretchToFit')) {
-      ResizeVideo(loadedVideoElement);
-    }
-  });
-
-  $('.stretchToFit').each(function() {
-    var CurrentChild = this;
-    ResizeVideo(CurrentChild);
+  $video.each( function () {
+  	if (this.readyState > 0) {
+  		configureStretchToFitVideo(this);
+  	} else {
+  		$video.bind("loadedmetadata", function(anEvent) {
+  			configureStretchToFitVideo(this);
+  		});
+  	}
   });
 });
